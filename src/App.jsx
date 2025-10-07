589e3dc091e5b4124af134e357bc4f687839b6df
darshan jain <darshan.jain.ndss@gmail.com>
2025-10-07 17:10:10 +0530

    task2

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import First from './component/First'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -21,6 +21,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <First/>
     </>
   )
 }
---
+++ src/component/First.jsx
@@ -0,0 +1,10 @@
+function First(){
+
+
+    return(<>
+    <h1>hello world</h1>
+    
+    </>)
+}
+
+export default First
